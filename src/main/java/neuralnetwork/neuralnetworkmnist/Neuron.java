0ab7b0fe3ad94e86fd4c5afe65e45ae49f53ab47
Ari Kauppi <ari.kauppi@santelo.fi>
2021-01-25 03:59:31 +0200

    Added basic functions to Neuron.java

--- src/main/java/neuralnetwork/neuralnetworkmnist/Neuron.java
+++ src/main/java/neuralnetwork/neuralnetworkmnist/Neuron.java
@@ -3,11 +3,77 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package neuralnetwork.neuralnetworkmnist;
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 /**
- *
+ * Neuron
+ * 
+ * 
  * @author ari
  */
 public class Neuron {
+
+    /**
+     * ArrayList of real numbers, which represent
+     * the input weights to this Neuron
+     */
+    private ArrayList<Double> listOfWeightsIn;
+
+    /**
+     * ArrayList of real numbers, which represent
+     * the output weights from this Neuron.
+     */
+    private ArrayList<Double> listOfWeightsOut;
+
+    /**
+     * Initializes listOfWeightsIn and
+     * listOfWeightsOut with random numbers.
+     */
+    public double initNeuron() {
+        // unimplemented
+        return 0.0;
+    }
+
+    /**
+     * Sets a list of real numbers (doubles) as the
+     * listOfWeightsIn
+     * @param listOfWeightIn
+     * @return None
+     */
+    public void setListOfWeightsIn(ArrayList<Double> listOfWeightIn) {
+        return None;
+    }
+
+    /**
+     * Sets a list of real numbers (doubles) as the listOfWeightsOut
+     * for a neuron.
+     * @param listOfWeightsOut
+     * @return None
+     */
+    public void setListOfWeightsOut(ArrayList<Double> listOfWeightsOut) {
+        return None;    
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Arraylist<Double> getListOfWeightsIn() {
+        return this.listOfWeightsIn;
+    }
+
+
+
+
+
     
+
 }
